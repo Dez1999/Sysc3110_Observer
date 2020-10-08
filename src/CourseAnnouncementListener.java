8e17842d0d29f7b169c915355526abb85471e564
Blake <blake_db99@outlook.com>
2020-10-08 19:40:40 -0400

    Observer Project

---
+++ src/CourseAnnouncementListener.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+/**
+ * Interface: methods with no implementation that define a contract. Contract is made up off lots of methods with no implementation
+ */
+
+public interface CourseAnnouncementListener {
+
+    void handleMidtermDateSet(Date date);
+
+
+}
